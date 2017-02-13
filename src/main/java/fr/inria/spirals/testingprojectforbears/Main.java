36283b6c7bb2fee70a06c403d5785c3d410ebad7
Fernanda Madeiral <fer.madeiral@gmail.com>
2017-02-13 17:05:23 +0100

    Fourth commit.

--- src/main/java/fr/inria/spirals/testingprojectforbears/Main.java
+++ src/main/java/fr/inria/spirals/testingprojectforbears/Main.java
@@ -3,7 +3,7 @@ package fr.inria.spirals.testingprojectforbears;
 public class Main {
 
 	public static int add(int a, int b) {
-		return a - b;
+		return a + b;
 	}
 	
 	public static void main(String[] args) {
