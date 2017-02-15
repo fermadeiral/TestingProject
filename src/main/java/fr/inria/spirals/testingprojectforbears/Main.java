860e91faae97322ecb22b76314fb74fb8e875091
Fernanda Madeiral <fer.madeiral@gmail.com>
2017-02-15 17:31:31 +0100

    A fixer commit for a unknown bug.

--- src/main/java/fr/inria/spirals/testingprojectforbears/Main.java
+++ src/main/java/fr/inria/spirals/testingprojectforbears/Main.java
@@ -3,7 +3,7 @@ package fr.inria.spirals.testingprojectforbears;
 public class Main {
 
 	public static int anything(int a, int b) {
-		if (a < 10) {
+		if (a <= 10) {
 			return a + b;
 		} else {
 			return a - b;
